a6cf6a0089a0584e5fdd8198d969d5fb48ca4a45
CNAnda1623 <cnanda162@gmail.com>
2026-03-16 18:48:54 +0530

    firebase credential update

--- react-firechat-master/src/App.js
+++ react-firechat-master/src/App.js
@@ -10,12 +10,12 @@ import Channel from './components/Channel';
 import Loader from './components/Loader';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCmQ6J4SGPNgls6Zoavw3RT-newPvICnNA",
-  authDomain: "firechat-8f664.firebaseapp.com",
-  projectId: "firechat-8f664",
-  storageBucket: "firechat-8f664.firebasestorage.app",
-  messagingSenderId: "884888996880",
-  appId: "1:884888996880:web:85cd69eba22f5e8a1a8e95"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 const MoonIcon = props => (
